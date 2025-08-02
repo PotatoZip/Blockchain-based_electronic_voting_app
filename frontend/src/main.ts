fac97bbbeaea8e44c0cf6dbaee58789c5726ed77
Wiktor <wiktor.zmiendak@gmail.com>
2025-08-02 18:07:14 +0200

    [feat] vite_tailwind_setup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+import './assets/tailwind.css'
+import './style.css'
+
+createApp(App).use(router).mount('#app')
